e8339751d86a21faf7adb778812016f341a232d4
RV <rauno.valk@voco.ee>
2024-11-22 17:17:10 +0200

    Ylesanne

---
+++ classes/Student.js
@@ -0,0 +1,42 @@
+const Person = require('./Person')
+class Student extends Person{
+    constructor(name){
+        super(name)
+        this.id = null
+        this.grades =[] 
+    } 
+
+    setId(id){
+        if(this.id === null){
+            this.id = id
+        } 
+    } 
+
+    getId(){
+        return this.id
+    }
+    addGrade(course, grade){
+        const newGrade ={
+            course: course,
+            grade: grade
+        } 
+        this.grades.push(newGrade)
+    } 
+
+    getGrades(){
+        return this.grades
+    }
+    getAverageGrades(){
+        if(this.grades.length === 0){ 
+            return -1
+    } else {
+        // todo
+        return 0
+        }  
+    } 
+    description(){
+        return `Student ${this.name}  `
+    } 
+} 
+
+module.exports = Student
